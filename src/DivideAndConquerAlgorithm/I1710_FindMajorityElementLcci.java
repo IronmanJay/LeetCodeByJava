86fe3890ed5fbee5db3dc0a1415797d679e7cefb
IronmanJay <1975686676@qq.com>
2020-08-04 09:44:17 +0800

    2020-08-04

---
+++ src/DivideAndConquerAlgorithm/I1710_FindMajorityElementLcci.java
@@ -0,0 +1,79 @@
+package DivideAndConquerAlgorithm;
+
+public class I1710_FindMajorityElementLcci {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 5, 9, 5, 9, 5, 5, 5};
+        int res = majorityElement(nums);
+        System.out.println("res = " + res);
+    }
+
+    public static int majorityElement(int[] nums) {
+        return majorityElementReturn(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * 使用分治算法查找每部分的最多元素
+     *
+     * @param nums  待查数组
+     * @param left  左指针
+     * @param right 右指针
+     * @return 当前部分的最多元素
+     */
+    public static int majorityElementReturn(int[] nums, int left, int right) {
+        // 如果分成了单个元素，直接返回，开始“治”
+        if (left == right) {
+            return nums[left];
+        }
+        // 这里使用二分法，开始“分”
+        int mid = (right - left) / 2 + left;
+        // 分别向左右递归，开始“分”，最终将数组分成单个元素
+        int leftNum = majorityElementReturn(nums, left, mid);
+        int rightNum = majorityElementReturn(nums, mid + 1, right);
+        // 如果分成当前部分单个元素，左边元素值和右边元素值相等，说明当前阶段的众数就是这个元素，返回即可，这也是“治”
+        if (leftNum == rightNum) {
+            return leftNum;
+        }
+        // 如果分成当前部分单个元素左边和右边不相等，那么就需要在当前数组中分别找到各自出现的次数
+        int leftNumCount = countInRange(nums, leftNum, left, mid);
+        int rightNumCount = countInRange(nums, rightNum, mid + 1, right);
+        // 这里记录当前左右数组的一半，如果当前元素大于当前数组的一半，那么肯定是这个数组的主要元素
+        int leftHalf = (mid - left) >> 1;
+        int rightHalf = (right - (mid + 1)) >> 1;
+        // 这里两个判断条件，首先是当前元素出现的次数要大于当前数组的一半，另一个是左边元素出现的次数要大于右边元素出现的次数，这样左边就是当前数组的主要元素
+        if (leftNumCount > leftHalf && leftNumCount > rightNumCount) {
+            return leftNum;
+        }
+        // 右边判断同上
+        if (rightNumCount > rightHalf && leftNumCount < rightNumCount) {
+            return rightNum;
+        }
+        // 正常情况下，当左边元素出现次数和右边元素出现次数相等时，返回左右元素均可，但这里为了避免特殊情况，例如[8,8,7,7,7]，这种情况左边8出现2次，右边7出现2次，看似出现次数一样，其实左边小数组是[8,8,7]，右边小数组是[7,7]，这样对右边小数组是不公平的，因为有一次7没算进去，所以当他们个数相等时，返回右边的
+        if (leftNumCount == rightNumCount) {
+            return rightNum;
+        }
+        // 如果上述情况都不满足，返回-1
+        return -1;
+    }
+
+    /**
+     * 返回当前部分数组某个元素出现的次数
+     *
+     * @param nums   部分数组
+     * @param target 目标值
+     * @param left   左指针
+     * @param right  右指针
+     * @return 当前部分数组target出现的次数
+     */
+    public static int countInRange(int[] nums, int target, int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
