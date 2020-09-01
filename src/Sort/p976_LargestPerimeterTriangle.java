3011d93c588ff230a4dbb2f000238068cb7d7fc9
IronmanJay <1975686676@qq.com>
2020-09-01 10:33:45 +0800

    2020-09-01

--- src/Sort/p976_LargestPerimeterTriangle.java
+++ src/Sort/p976_LargestPerimeterTriangle.java
@@ -9,10 +9,13 @@ public class p976_LargestPerimeterTriangle {
     }
 
     public static int largestPerimeter(int[] A) {
+        // 判空返回
         if (A == null || A.length == 0) {
             return 0;
         }
+        // 对数组进行排序
         quickSort(A, 0, A.length - 1);
+        // 从后向前比较，如果发现两边之和大于第三边即可，因为从后向前找，所以只要有满足的就是最大的
         for (int i = A.length - 1; i >= 2; i--) {
             if (A[i] < A[i - 1] + A[i - 2]) {
                 return A[i] + A[i - 1] + A[i - 2];
@@ -21,6 +24,7 @@ public class p976_LargestPerimeterTriangle {
         return 0;
     }
 
+    // 快速排序
     public static void quickSort(int[] arr, int low, int high) {
         if (low >= high) {
             return;
