2bb01a1eff24c9a7b0d47430376ba56c1a7bfa50
IronmanJay <1975686676@qq.com>
2022-11-03 09:31:31 +0800

    2022-11-03

---
+++ src/Queue/p1823_FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,27 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class p1823_FindTheWinnerOfTheCircularGame {
+
+    public static void main(String[] args) {
+        int res = findTheWinner(5, 2);
+        System.out.println("res = " + res);
+    }
+
+    public static int findTheWinner(int n, int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+        while (queue.size() > 1) {
+            for (int i = 1; i < k; i++) {
+                queue.offer(queue.poll());
+            }
+            queue.poll();
+        }
+        return queue.poll();
+    }
+
+}
