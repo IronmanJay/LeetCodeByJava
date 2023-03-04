1ac819691438ab9116da847db2f2c24d22d33a3d
IronmanJay <1975686676@qq.com>
2023-03-04 14:55:56 +0800

    2023-03-04

---
+++ src/GreedyAlgorithm/p605_CanPlaceFlowers.java
@@ -0,0 +1,28 @@
+package GreedyAlgorithm;
+
+/**
+ * @Author: IronmanJay
+ * @Description: TODO
+ * @CreateTime: 2023-03-04  14:27
+ */
+public class p605_CanPlaceFlowers {
+
+    public static void main(String[] args) {
+        int[] flowered = {1, 0, 0, 0, 1};
+        int n = 1;
+        boolean res = canPlaceFlowers(flowered, n);
+        System.out.println("res = " + res);
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int res = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && ((i + 1) == flowerbed.length || flowerbed[i + 1] == 0) && (i == 0 || flowerbed[i - 1] == 0)) {
+                flowerbed[i] = 1;
+                res++;
+            }
+        }
+        return res >= n;
+    }
+
+}
