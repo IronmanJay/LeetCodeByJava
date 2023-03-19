1968ec046e03ea671bf5eaf5edecbab86d3a5953
IronmanJay <1975686676@qq.com>
2023-03-19 09:57:40 +0800

    2023-03-19

--- src/DoublePointer/p165_CompareVersionNumbers.java
+++ src/DoublePointer/p165_CompareVersionNumbers.java
@@ -20,13 +20,13 @@ public class p165_CompareVersionNumbers {
         while (index1 < version1.length() || index2 < version2.length()) {
             int x = 0;
             while (index1 < version1.length() && version1.charAt(index1) != '.') {
-                x += x * 10 + version1.charAt(index1) - '0';
+                x = x * 10 + version1.charAt(index1) - '0';
                 index1++;
             }
             index1++;
             int y = 0;
             while (index2 < version2.length() && version2.charAt(index2) != '.') {
-                y += y * 10 + version2.charAt(index2) - '0';
+                y = y * 10 + version2.charAt(index2) - '0';
                 index2++;
             }
             index2++;
